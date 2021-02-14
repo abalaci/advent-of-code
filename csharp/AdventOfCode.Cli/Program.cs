9c8baffb0a9684ebe3ed92dfb7664ad0d7f9bee2
abalaci <47945400+abalaci@users.noreply.github.com>
2021-02-14 22:38:34 +0200

    Start CLI implementation

--- csharp/AdventOfCode.Cli/Program.cs
+++ csharp/AdventOfCode.Cli/Program.cs
@@ -1,12 +1,39 @@
 using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using AdventOfCode.Y2015;
 
 namespace AdventOfCode.Cli
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var rootCommand = new RootCommand
+            {
+                new Option<uint>("--year"),
+                new Option<uint>("--day"),
+                new Option<FileInfo>("--input-file"),
+            };
+
+            rootCommand.Description = "My sample Advent of Code puzzle solver.";
+            rootCommand.Handler = CommandHandler.Create<uint, uint, FileInfo>((year, day, inputFile) =>
+            {
+                Console.WriteLine($"Year: {year}.");
+                Console.WriteLine($"Day: {day}.");
+                Console.WriteLine($"Input file info: {inputFile}.");
+
+                using StreamReader streamReader = inputFile.OpenText();
+                string input = streamReader.ReadToEnd();
+
+                // TODO: Select puzzle dynamically based on year and day arguments.
+                var puzzle = new NotQuiteLispPuzzle(input.Trim());
+                Console.WriteLine($"Part one answer: {puzzle.SolvePartOne()}.");
+                Console.WriteLine($"Part two answer: {puzzle.SolvePartTwo()}.");
+            });
+
+            rootCommand.Invoke(args);
         }
     }
 }
